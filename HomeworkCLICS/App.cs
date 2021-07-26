dc64f755ff7f4071438b1c57443b7f5755356f32
Herrscher <go.eval@yandex.com>
2021-07-26 21:41:54 +0800

    update version

--- HomeworkCLICS/App.cs
+++ HomeworkCLICS/App.cs
@@ -2,7 +2,7 @@ namespace HomeworkCLI
 {
     public class App
     {
-        public static string Version = "2.0.0-alpha";
-        public static int VersionCode = 202105271;
+        public static string Version = "2.0.1-alpha";
+        public static int VersionCode = 202107261;
     }
 }
